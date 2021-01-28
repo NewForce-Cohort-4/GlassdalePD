fdc9a82591e6f6c3f1f813bfc26e026ecda6583a
Jordan Castelloe <jordan.castelloe@gmail.com>
2021-01-28 10:49:25 -0500

    added event listener for conviction filter

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -3,6 +3,7 @@
  *   which lists all convictions in the Glassdale PD API
  */
 import { getConvictions, useConvictions } from "./ConvictionProvider.js";
+import {CriminalList} from "../criminals/CriminalList.js"
 
 // Get a reference to the DOM element where the <select> will be rendered
 const contentTarget = document.querySelector(".convictions-select-container");
@@ -26,8 +27,35 @@ const render = (convictionsCollection) => {
             <option value="0">Please select a crime...</option>
             ${convictionsCollection.map((currentConvictionInLoop) => {
                 const crimeName = currentConvictionInLoop.name
-                return `<option value="0">${crimeName}</option>`
+                return `<option>${crimeName}</option>`
             })}
         </select>
     `;
 };
+
+// This won't throw an error, but it will fire any time there's a change event anywhere in the main container
+const eventHub = document.querySelector("main")
+eventHub.addEventListener("change", (eventObject) => {
+    console.log("You clicked somewhere in the main container")
+    console.log(eventObject)
+
+    // To be more specific, we need to know specifically what we clicked on
+    console.log("Here is the element you clicked on: ", eventObject.target.id)
+
+    if(eventObject.target.id === "crimeSelect"){
+        console.log("You selected something from the crime dropdown")
+        console.log("This is the crime that was selected: ", eventObject.target.value)
+        // Your code goes here!
+          /*
+        - When we select a crime, we need to filter the criminals in CriminalList.
+        - Start by importing the CriminalList component at the top of this file. - CHECK
+        - Then call CriminalList - CHECK
+        // and pass in information about the crime that was chosen
+        */
+       const crimeChosen = eventObject.target.value
+       CriminalList(crimeChosen)
+    }
+
+})
+
+
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,24 +1,37 @@
 import { Criminal } from './Criminal.js';
 import { getCriminals, useCriminals } from './CriminalDataProvider.js'
 
-export const CriminalList = () => {
+export const CriminalList = (crimeThatWasChosen) => {
+
     getCriminals().then(() => {
         // criminals is an array of objects
-        let criminals = useCriminals();
-        // Now that you have the data, what component should be rendered?
+        let criminalsToPrint = useCriminals();
+
+
+         // If we get input from the convictions filter, filter our criminals so that we only see ones with that conviction
+      if(crimeThatWasChosen){
+
+
+       criminalsToPrint = criminalsToPrint.filter(currentCriminal => {
+            return currentCriminal.conviction === crimeThatWasChosen
+        })
+
+
+
+        // criminalsToPrint = criminalsThatCommittedSpecificCrime
+
+      }
         const criminalsContainer = document.querySelector(".criminal-list")
 
         let htmlBuilder = ""
 
-        for(let i =0; i < criminals.length; i++){
+        for(let i =0; i < criminalsToPrint.length; i++){
 
-            htmlBuilder += Criminal(criminals[i])
+            htmlBuilder += Criminal(criminalsToPrint[i])
 
         }
 
         criminalsContainer.innerHTML = htmlBuilder
-
-
 
     })
 }
@@ -26,3 +39,25 @@ export const CriminalList = () => {
 
 
 document.querySelector("#criminals-nav-link").addEventListener("click", CriminalList)
+
+
+// export const CriminalList = (convictionFilter) => {
+//     let criminalListContainer = document.querySelector(".criminal-list");
+
+//     criminalListContainer.innerHTML = ""
+
+//     getCriminals().then(() => {
+//       let criminals = useCriminals();
+
+//       // If we get input from the convictions filter, filter our criminals so that we only see ones with that conviction
+//       if(convictionFilter){
+
+//         criminals = // write your filter here
+
+//       }
+
+//       criminals.forEach((singleCriminal) => {
+//         criminalListContainer.innerHTML += Criminal(singleCriminal);
+//       });
+//     });
+//   };
